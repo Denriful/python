fcedb8324431d95bd438f8c3d7ff6e46deee475d
Denis Sulgin <sulgin.ds@med54.ru>
2019-07-31 15:10:03 +0700

    new file with SETS example

---
+++ Множества/vowels7.py
@@ -0,0 +1,15 @@
+vowels = {"a","e","i","o","u"}
+#word = "Milliways"
+word = input("Provide a word to search for vowels: ")
+
+print(vowels.intersection(set(word)))
+
+#found = []
+#for letter in word:
+#    if letter in vowels:
+#        print(letter)
+#        if letter not in found:
+#            found.append(letter)
+
+#for vowel in found:
+#    print(vowel)
