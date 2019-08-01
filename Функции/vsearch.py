90fcc828c4cfbdd93556006d77e7b70b8da92acf
Denis Sulgin <sulgin.ds@med54.ru>
2019-08-01 14:06:51 +0700

    add arg to func

--- Функции/vsearch.py
+++ Функции/vsearch.py
@@ -1,11 +1,13 @@
-def search4vowels ():
-# print vowesl in provided word
+def search4vowels (word):
+
+    """ This func search for vowels in word."""
+
     """ another type of comment. 
         multistring
     """
     vowels = set('aeiou')
 
-    word = input('Provide a word to search for vowels: ')
+    #word = input('Provide a word to search for vowels: ')
 
     found = vowels.intersection(set(word))
 
