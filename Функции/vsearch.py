5311391d22905bbf232bc70d20b63fc3b025b223
Denis Sulgin <sulgin.ds@med54.ru>
2019-08-02 09:02:44 +0700

    add func 'search4letters'

--- Функции/vsearch.py
+++ Функции/vsearch.py
@@ -16,3 +16,7 @@ def search4vowels(phrase:str) -> set:
     #    print(vowel)
 
     #return bool(found)
+
+
+def search4letters(phrase:str, letters:str) -> set:
+    return set(letters).intersection(set(phrase))
