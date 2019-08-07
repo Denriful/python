6ea2a2376b70aef78d86bc570e72d53139dd8399
Denis Sulgin <sulgin.ds@med54.ru>
2019-08-07 08:17:17 +0700

    add setup.py

--- mymodules/setup.py
+++ mymodules/setup.py
@@ -4,9 +4,9 @@ from setuptools import setup
 setup(
     name='vsearch',
     version='1.0',
-    description='The Head First Python Search Tools'
-    author='Denriful'
-    author_email='sobran68@mail.ru'
-    url='med54.ru'
+    description='The Head First Python Search Tools',
+    author='Denriful',
+    author_email='sobran68@mail.ru',
+    url='med54.ru',
     py_modules=['vsearch']
 )
