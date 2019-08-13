f92d60c4ea4f3c401bff06389bcc2ba03128e60a
Denis Sulgin <sulgin.ds@med54.ru>
2019-08-13 16:45:55 +0700

    add vseach module to hello_flask

--- webapp/hello_flask.py
+++ webapp/hello_flask.py
@@ -1,9 +1,17 @@
 from flask import Flask
 
+from vsearch import search4letters
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello() -> str:
     return 'Hello world from Flask!'
+
+#letters = search4letters('Life, the Universe, and Everything!', 'eiour')
+
+@app.route('/search4')
+def do_search() -> str:
+    return str(search4letters('Life, the Universe, and Everything!', 'eiour'))
 
 app.run()
