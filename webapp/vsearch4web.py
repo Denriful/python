0ef5d2bf780aec20265499d2c74d30e4c5577cef
Denis Sulgin <sulgin.ds@med54.ru>
2019-08-20 10:32:24 +0700

    comment '- > ' cause gunicorn don't start

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -12,7 +12,8 @@ app = Flask(__name__)
 #letters = search4letters('Life, the Universe, and Everything!', 'eiour')
 
 @app.route('/search4', methods=['POST'])
-def do_search() -> 'html':
+#def do_search() -> 'html':
+def do_search():
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
@@ -28,7 +29,8 @@ def do_search() -> 'html':
 
 @app.route('/')
 @app.route('/entry')
-def entry_page() -> 'html':
+#def entry_page() -> 'html':
+def entry_page():
     return render_template('entry.html', 
         the_title='Welcome to search4letters on the web!')
 
