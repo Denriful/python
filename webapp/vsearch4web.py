bc7dcc64558d8b7dacf0f2d0c4ffbb51515fd9c2
Denis Sulgin <sulgin.ds@med54.ru>
2019-08-22 10:36:21 +0700

    add log file and view in HTML

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,23 +1,27 @@
 from flask import Flask
 
-from flask import render_template, request
+from flask import render_template, request, escape
 
 """ relative import for 
 support Heroku
+annotations disabled for compatibility with Heroku
 """
 from vsearch import search4letters
-#from .vsearch import search4letters
+
 app = Flask(__name__)
 
 #letters = search4letters('Life, the Universe, and Everything!', 'eiour')
 
 @app.route('/search4', methods=['POST'])
+
+
 #def do_search() -> 'html':
 def do_search():
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
 
     return render_template('results.html', 
         the_phrase=phrase, 
@@ -33,6 +37,28 @@ def do_search():
 def entry_page():
     return render_template('entry.html', 
         the_title='Welcome to search4letters on the web!')
+
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log','a') as log:
+
+        """ this is how to print 'flask request' attributes """
+        #print(dir(req),res, file=log)
+
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+
+@app.route('/viewlog')
+def view_the_log() -> str:
+    contents = []
+    with open('vsearch.log') as log:        
+        for line in log:       
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+        #contents = log.readlines()
+        #return escape(''.join(contents))
+        return str(contents)
+
+
 
 if __name__ == "__main__":    
     app.run(debug=True)
