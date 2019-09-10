2c7101b152976797ef995a3dcb9541f23bd7f62a
Denis Sulgin <sulgin.ds@med54.ru>
2019-09-10 13:07:35 +0700

    add vsearch + sql

--- webapp/vsearch4web_sql.py
+++ webapp/vsearch4web_sql.py
@@ -1,21 +1,24 @@
-from flask import Flask
+#! /usr/bin/env python3
 
-from flask import render_template, request, escape
+from flask import Flask, render_template, request, escape
 
-""" relative import for 
-support Heroku
-annotations disabled for compatibility with Heroku
-"""
 from vsearch import search4letters
+
+import mysql.connector
+
+dbconfig = { 'host': '127.0.0.1', 
+             'user': 'vsearch', 
+             'password': '34512',
+             'database': 'vsearhlogDB', }
+
+conn = mysql.connector.connect(**dbconfig)
+
+cursor = conn.cursor()
 
 app = Flask(__name__)
 
-#letters = search4letters('Life, the Universe, and Everything!', 'eiour')
-
 @app.route('/search4', methods=['POST'])
 
-
-#def do_search() -> 'html':
 def do_search():
     phrase = request.form['phrase']
     letters = request.form['letters']
@@ -28,23 +31,49 @@ def do_search():
         the_letters=letters,
         the_title=title,
         the_results=results,)    
-    #return str(search4letters(phrase, letters))
-    #return str(search4letters('Life, the Universe, and Everything!', 'eiour'))
-
+ 
 @app.route('/')
 @app.route('/entry')
-#def entry_page() -> 'html':
+
 def entry_page():
     return render_template('entry.html', 
         the_title='Welcome to search4letters on the web!')
 
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log','a') as log:
+    
+    _SQL = '''insert into log
+        (phrase, letters, ip, browser_string, results)
+        values
+        (%s, %s, %s, %s, %s)'''
 
-        """ this is how to print 'flask request' attributes """
+    cursor.execute(_SQL, (req.form['phrase'],
+        req.form['letters'], 
+        req.remote_addr,
+        req.user_agent.browser,
+        res, ))
+    
+    conn.commit()
+    
+   # _SQL = '''select * from log'''
+
+    #for row in cursor.fetchall():
+    #    print(row)
+
+    cursor.close()
+    
+    conn.close()
+
+
+
+    #with open('vsearch.log','a') as log:
+
+    """ this is how to print 'flask request' attributes """
         #print(dir(req),res, file=log)
 
-        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+        #print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+        
+
+
 
 @app.route('/viewlog')
 def view_the_log() -> 'html':
