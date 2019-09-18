4d2099c732aa4ec4bf3aa301ab7f45c0f3e849d5
Denis Sulgin <sulgin.ds@med54.ru>
2019-09-18 15:13:18 +0700

    fix @app.route in quick_session.py

--- webapp/quick_session.py
+++ webapp/quick_session.py
@@ -4,15 +4,14 @@ app = Flask(__name__)
 
 app.secret_key = 'YouWillNeverGuess'
 
-app.route('/setuser/<user>')
+@app.route('/setuser/<user>')
 def setuser(user: str) -> str:
     session['user'] = user
     return 'User value set to: ' + session['user']
 
-app.route('/getuser')
+@app.route('/getuser')
 def getuser() -> str:
     return 'User value is currently set to: ' + session['user']
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
