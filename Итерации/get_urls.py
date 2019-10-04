f569f0d3818d440790b13fbbe3b4b9d0e61ddb3e
Denis Sulgin <sulgin.ds@med54.ru>
2019-10-04 15:59:54 +0700

    some comments to code

--- Итерации/get_urls.py
+++ Итерации/get_urls.py
@@ -1,6 +1,19 @@
+''' use function-generator '''
+
 from urls_utils import gen_from_urls
+
+import pprint
 
 urls = ('http://ngs.ru', 'http://ya.ru', 'http://med54.ru')
 
 for  resp_len, status, url in gen_from_urls(urls):
     print(resp_len, '->', status, '->', url)
+
+print()
+
+
+'''func gen inside dict gen'''
+
+urls_res = {url: size for size, _, url in gen_from_urls(urls)}
+
+pprint.pprint(urls_res)
